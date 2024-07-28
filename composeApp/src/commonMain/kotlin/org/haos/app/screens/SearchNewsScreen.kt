503e91c0bcc1caff91f9d7c354672032c5a0b414
Exxuslee <exxuslee@gmail.com>
2024-07-28 22:29:04 +0300

    happy birthday

--- composeApp/src/commonMain/kotlin/org/haos/app/screens/SearchNewsScreen.kt
+++ composeApp/src/commonMain/kotlin/org/haos/app/screens/SearchNewsScreen.kt
@@ -1,11 +1,21 @@
 package org.haos.app.screens
 
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import cafe.adriel.voyager.core.screen.Screen
+import org.haos.app.ui.title2_leah
 
 class SearchNewsScreen : Screen {
     @Composable
     override fun Content() {
-
+        title2_leah(
+            "C Днем Рождения, Глеб!",
+            modifier = Modifier.fillMaxSize().padding(48.dp),
+            textAlign = TextAlign.Center
+        )
     }
 }
