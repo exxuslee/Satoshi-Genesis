a4419f436bcbaa4866bc15ec20b256a27c134ffd
Exxuslee <exxuslee@gmail.com>
2024-08-01 22:00:50 +0300

    add string-ru

--- composeApp/src/commonMain/kotlin/org/haos/app/App.kt
+++ composeApp/src/commonMain/kotlin/org/haos/app/App.kt
@@ -4,9 +4,7 @@ import androidx.compose.animation.Crossfade
 import androidx.compose.material.BottomNavigation
 import androidx.compose.material.BottomNavigationItem
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.outlined.FavoriteBorder
 import androidx.compose.material.icons.outlined.Settings
-import androidx.compose.material.icons.rounded.Favorite
 import androidx.compose.material.icons.rounded.Settings
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
@@ -15,7 +13,6 @@ import androidx.compose.runtime.*
 import cafe.adriel.voyager.navigator.Navigator
 import org.haos.app.navigations.ScreenGraph
 import org.haos.app.screens.PuzzleScreen
-import org.haos.app.screens.QubitScreen
 import org.haos.app.screens.SettingsScreen
 import org.haos.app.screens.WalletScreen
 import org.haos.app.theme.AppTheme
@@ -51,7 +48,6 @@ internal fun App() = AppTheme {
             when (selectedScreen) {
                 ScreenGraph.Wallet -> Navigator(WalletScreen())
                 ScreenGraph.Puzzle -> Navigator(PuzzleScreen())
-                ScreenGraph.Qubit -> Navigator(QubitScreen())
                 ScreenGraph.Settings -> Navigator(SettingsScreen())
             }
         }
@@ -62,11 +58,9 @@ internal fun App() = AppTheme {
 private fun getIconForScreen(screen: ScreenGraph, selected: Boolean) = if (selected) when (screen) {
     ScreenGraph.Wallet -> vectorResource(Res.drawable.ic_account_balance_fill1)
     ScreenGraph.Puzzle -> vectorResource(Res.drawable.ic_sports_fill1)
-    ScreenGraph.Qubit -> Icons.Rounded.Favorite
     ScreenGraph.Settings -> Icons.Rounded.Settings
 } else when (screen) {
     ScreenGraph.Wallet -> vectorResource(Res.drawable.ic_account_balance_fill0)
     ScreenGraph.Puzzle -> vectorResource(Res.drawable.ic_sports_fill0)
-    ScreenGraph.Qubit -> Icons.Outlined.FavoriteBorder
     ScreenGraph.Settings -> Icons.Outlined.Settings
 }
--- composeApp/src/commonMain/kotlin/org/haos/app/navigations/ScreenGraph.kt
+++ composeApp/src/commonMain/kotlin/org/haos/app/navigations/ScreenGraph.kt
@@ -1,8 +1,11 @@
 package org.haos.app.navigations
 
 import org.jetbrains.compose.resources.StringResource
-import satoshi_genesis.composeapp.generated.resources.*
+import satoshi_genesis.composeapp.generated.resources.Res
+import satoshi_genesis.composeapp.generated.resources.puzzle
+import satoshi_genesis.composeapp.generated.resources.settings
+import satoshi_genesis.composeapp.generated.resources.wallet
 
 enum class ScreenGraph(val label: StringResource) {
-    Wallet(Res.string.wallet), Puzzle(Res.string.puzzle), Qubit(Res.string.qubit), Settings(Res.string.settings),
+    Wallet(Res.string.wallet), Puzzle(Res.string.puzzle), Settings(Res.string.settings),
 }
