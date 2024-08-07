efc90632923779b9a54fabbbc5f1e28584871bd0
Exxuslee <exxuslee@gmail.com>
2024-08-08 00:40:03 +0300

    WebJs: add tonconnect-ui

--- composeApp/src/jsMain/kotlin/org/haos/app/screens/wallet/WalletScreenModel.js.kt
+++ composeApp/src/jsMain/kotlin/org/haos/app/screens/wallet/WalletScreenModel.js.kt
@@ -8,7 +8,7 @@ internal actual fun connectTon() {
     println("ConnectTon for JVM")
     val script = """
     const tonConnectUI = new TON_CONNECT_UI.TonConnectUI({
-        manifestUrl: 'https://https://exxuslee.github.io/Satoshi-Genesis/tonconnect-manifest.json'
+        manifestUrl: 'https://exxuslee.github.io/Satoshi-Genesis/tonconnect-manifest.json'
     });
 
     tonConnectUI.uiOptions = {
--- docs/connectTon.js
+++ docs/connectTon.js
@@ -1,6 +1,6 @@
 
 const tonConnectUI = new TON_CONNECT_UI.TonConnectUI({
-    manifestUrl: 'https://https://exxuslee.github.io/Satoshi-Genesis/tonconnect-manifest.json'
+    manifestUrl: 'https://exxuslee.github.io/Satoshi-Genesis/tonconnect-manifest.json'
 });
 
 tonConnectUI.uiOptions = {
